7fc9031ecd0bc1ac83b228000e618f2c550954a3
Pedro Florit Docavo <pedroflorit@msn.com>
2025-01-12 17:40:34 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -1,24 +1,24 @@
-const palos = ["Corazones", "Diamantes", "Tréboles", "Picas"];
-const valores = [
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "J",
-  "Q",
-  "K",
-  "A",
-];
-const cartaDiv = document.getElementById("carta");
-const generarCartaBtn = document.getElementById("generarCartaBtn");
+function cardGenerate() {
+  let suits = ["♥", "♦", "♠", "♣"];
+  let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  // Index random generator
+  let indexSuits = Math.floor(Math.random() * suits.length);
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  // Rendering randomly suits and numbers
+  document.querySelector("#top").innerHTML = suits[indexSuits];
+  document.querySelector("#bottom").innerHTML = suits[indexSuits];
+  document.querySelector("#number").innerHTML = numbers[indexNumbers];
+  // Rendering suit color  
+  let isRed = (suits[indexSuits] == "♦" || suits[indexSuits] == "♥");
+  document.querySelector("#top").className = isRed ? "text-danger" : "text-dark";
+  document.querySelector("#bottom").className = isRed ? "text-danger" : "text-dark";
+  document.querySelector("#number").className = isRed ? "text-danger" : "text-dark";
+}
 
-generarCartaBtn.addEventListener("click", () => {
-  const palo = palos[Math.floor(Math.random() * palos.length)];
-  const valor = valores[Math.floor(Math.random() * valores.length)];
-  cartaDiv.textContent = `${valor} de ${palo}`;
-});
+window.onload = function () { 
+  cardGenerate();
+  // Button Card Generator
+  document.querySelector("#btnNewCard").addEventListener("click", cardGenerate);
+};
+
+setInterval(cardGenerate, 1000);
